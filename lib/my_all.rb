c5e967c2bdb6282e28dc9e3599d90ebf68a2183b
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-24 07:13:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  values = []
   while i < collection.length 
-    yield(collection[i])
+   values << yield(collection[i])
     i = i + 1
   end
+  
+  if values
+  
 end
